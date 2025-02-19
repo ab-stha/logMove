ed30e6f0494e0127d276d8c9d61b326959df9b14
Abhiparth Shrestha <abhiparth@hotmail.co.uk>
2025-02-19 22:21:25 +0000

    Install and import expo file system; Add barebones button to 'Add new log' which saves a hard coded new log to the device's file system.

--- log-move/app/(tabs)/new-log.tsx
+++ log-move/app/(tabs)/new-log.tsx
@@ -1,6 +1,23 @@
 import { View, Text, TextInput, TouchableOpacity, FlatList, StyleSheet } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+
+
 
 export default function NewLogScreen() {
+
+  // define the uri of the file stored in the local storage
+  const filePath = FileSystem.documentDirectory + 'workouts.json';
+
+  let testLog = 'test log 1';
+
+  const handleNewLog = async function () {
+    const newLog = await FileSystem.writeAsStringAsync(filePath, testLog);
+
+    const displayLog = await FileSystem.readAsStringAsync(filePath);
+
+    console.log('Your new log is: ', displayLog);
+  };
+
   return (
     <View style={styles.container}>
         
@@ -11,9 +28,23 @@ export default function NewLogScreen() {
         </View>
       </View>
 
+      <View style = {styles.navButtonsContainer}>
+        <TouchableOpacity style = {styles.navButton} onPress = {handleNewLog}>
+            <Text style = {{color: '#f5f5f5', fontSize: 26}}>
+              New log
+            </Text>
+            <Text style = {{color: '#f8e4cd', fontSize: 17}}>
+              Log a new workout
+            </Text>
+        </TouchableOpacity>
+
+      </View>
+      
+
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -42,5 +73,17 @@ const styles = StyleSheet.create({
     color: '#f5f5f5',
     fontSize: 30
   },
-
+  navButtonsContainer: {
+    width: '100%',
+    alignItems: 'center',
+    paddingTop: 20,
+  },
+  navButton: {
+    backgroundColor: 'black',
+    padding: 15,
+    borderRadius: 10,
+    width: '80%',
+    alignItems: 'center',
+    marginBottom: 20
+  }
 });
